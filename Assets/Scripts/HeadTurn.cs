bc0795f8c7357aacbf43cae5fcc000be1f9e8c3c
asorokin111 <Artem.Sorokin@vuoksi.fi>
2023-05-17 10:58:14 +0300

    angle clamping (doesn't work)

--- Assets/Scripts/HeadTurn.cs
+++ Assets/Scripts/HeadTurn.cs
@@ -9,6 +9,16 @@ public class HeadTurn : MonoBehaviour
     [SerializeField]
     private float _searchRadius = 10f;
 
+    [Header("Minimum and maximum rotation values")]
+    [SerializeField]
+    private float xMax = 30f;
+    [SerializeField]
+    private float yMax = 80f;
+    [SerializeField]
+    private float xMin = -15f;
+    [SerializeField]
+    private float yMin = -80f;
+
     private void FixedUpdate()
     {
         GameObject player = FindPlayerInRadius(_searchRadius);
@@ -16,7 +26,11 @@ public class HeadTurn : MonoBehaviour
         {
             //_neckBone.LookAt(player.transform); // Might need Quaternion.Slerp
             //_neckBone.rotation = Quaternion.RotateTowards(_neckBone.transform.rotation, player.transform.rotation, Time.deltaTime * 100);
-            _neckBone.rotation = Quaternion.Slerp(_neckBone.rotation, Quaternion.LookRotation(player.transform.position - _neckBone.position), Time.deltaTime * 3);
+            Quaternion looking = Quaternion.LookRotation(player.transform.position - _neckBone.position);
+            Debug.Log(looking.eulerAngles);
+            looking.eulerAngles = ClampAngles(looking);
+            Debug.Log(looking.eulerAngles);
+            _neckBone.rotation = Quaternion.Slerp(_neckBone.rotation, looking, Time.deltaTime * 3);
         }
     }
 
@@ -32,5 +46,44 @@ public class HeadTurn : MonoBehaviour
             }
         }
         return null;
+    }
+
+    // Set X and Y of rotation to be within certain bounds
+    public Vector3 ClampAngles(Quaternion rotation)
+    {
+        Vector3 angles = rotation.eulerAngles;
+        float newX = Mathf.Clamp(angles.x, xMin, xMax);
+        float newY = Mathf.Clamp(angles.y, yMin, yMax);
+        // Clamp X
+        //if (angles.x > xMax)
+        //{
+        //    newX = xMax;
+        //}
+        //else if (angles.x < xMin)
+        //{
+        //    newX = xMin;
+        //}
+        //else
+        //{
+        //    newX = angles.x;
+        //}
+
+        //// Clamp Y
+        //if (angles.y > yMax)
+        //{
+        //    newY = yMax;
+        //}
+        //else if (angles.y < yMin)
+        //{
+        //    newY = yMin;
+        //}
+        //else
+        //{
+        //    newY = angles.y;
+        //}
+
+        angles = new Vector3(newX, newY, angles.z);
+
+        return angles;
     }
 }
