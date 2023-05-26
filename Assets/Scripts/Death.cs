2ede76b647560bc1c9d56ef261e5be86b254f194
asorokin111 <Artem.Sorokin@vuoksi.fi>
2023-05-26 12:31:46 +0300

    screamer

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -6,13 +6,22 @@ using UnityEngine.SceneManagement;
 public class Death : MonoBehaviour
 {
     [SerializeField] private string deathRoomName;
+    [SerializeField] private AudioSource _screamerSource;
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject);
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(deathRoomName);
+            _screamerSource.Play();
+            StartCoroutine(delayedLoadDeathScene(1.0f));
         }
+    }
+
+    // Load the death scene after a delay
+    private IEnumerator delayedLoadDeathScene(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(deathRoomName);
     }
 }
