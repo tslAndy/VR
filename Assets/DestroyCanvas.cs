578af66fb643221fda6b5dccde66867e58e06167
ilonk <Anna-Ilona.Nikolaeva@vuoksi.fi>
2023-05-26 09:50:51 +0300

    Note's images added

---
+++ Assets/DestroyCanvas.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DestroyCanvas : MonoBehaviour
+{
+    public void DestroyTheCanvas()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/NoteManager.cs
+++ Assets/Scripts/NoteManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class NoteManager : MonoBehaviour
 {
@@ -12,12 +13,17 @@ public class NoteManager : MonoBehaviour
     TMP_Text notesText;
     [SerializeField]
     NavMeshAgent shrek;
+    [SerializeField] private GameObject[] notesCanvas;
 
     public void AddNote(GameObject note)
     {
         TotalNotes++;
         ChangeText();
         note.SetActive(false);
+        if (notesCanvas[TotalNotes - 1].gameObject != null)
+        {
+            notesCanvas[TotalNotes - 1].SetActive(true);
+        }
         switch (TotalNotes)
         {
             case 3:
